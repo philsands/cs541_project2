d03c32ead00ee38881363855f2a0640c59073bfa
Phil Sands <psands@purdue.edu>
2015-03-07 18:25:25 -0500

    Empty skeleton file for HeapFile class

---
+++ HeapFile/src/heap/HeapFile.java
@@ -0,0 +1,55 @@
+package heap;
+/**
+ * 
+ * Phil Sands, Qiaomu Yao
+ * CS 541 - Project 2
+ * March 9, 2015
+ *
+ */
+
+public class HeapFile {
+
+	public HeapFile(String name) {
+		
+	}
+
+	public RID insertRecord(byte[] record)
+	{
+		
+	}
+	
+	public Tuple getRecord(RID rid)
+	{
+		
+	}
+	
+	public void updateRecord(RID rid, Tuple newRecord)
+	{
+		
+	}
+	
+	public void deleteRecord(RID rid)
+	{
+		
+	}
+	
+	public int getRecCnt()
+	{
+		
+	}
+	
+	public HeapScan openScan()
+	{
+		
+	}
+}
+
+class RID
+{
+	
+}
+
+class Tuple
+{
+	
+}
