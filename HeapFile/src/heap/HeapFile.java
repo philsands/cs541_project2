03be27fdb4678507f350b38b9c25d34a479d8da1
Phil Sands <psands@purdue.edu>
2015-03-07 19:34:09 -0500

    Devised member variables for class, constructor, and preliminary insertRecord method

--- HeapFile/src/heap/HeapFile.java
+++ HeapFile/src/heap/HeapFile.java
@@ -1,4 +1,9 @@
 package heap;
+
+import java.util.LinkedList;
+
+import global.RID;
+
 /**
  * 
  * Phil Sands, Qiaomu Yao
@@ -8,26 +13,56 @@ package heap;
  */
 
 public class HeapFile {
+	
+	private String HFName;
+	private LinkedList<HFPage> HFPageDirectory;	// change this to an appropriate data structure in order to improve performance
 
 	public HeapFile(String name) {
-		
+		HFName = name;
+		HFPageDirectory = new LinkedList<HFPage>();
 	}
 
+	// must be done in O(log(n))
 	public RID insertRecord(byte[] record)
 	{
+		HFPage insertPage = null;
+		RID rid = null;
+		if (HFPageDirectory.isEmpty())
+		{
+			insertPage = new HFPage();
+			rid = insertPage.insertRecord(record);
+			HFPageDirectory.add(insertPage);
+		}
+		else
+		{
+			// find the first page in the directory that has enough space for the given record
+			// NOTE: will need to update this in order to reach the logarithmic time expectation
+			for (HFPage hfp : HFPageDirectory)
+			{
+				if (hfp.getFreeSpace() >= record.length)
+				{
+					insertPage = hfp;
+					break;
+				}
+			}
+			rid = insertPage.insertRecord(record);
+		}
 		
+		return rid;
 	}
 	
 	public Tuple getRecord(RID rid)
 	{
-		
+		return new Tuple();
 	}
-	
+
+	// must be done in O(log(n))
 	public void updateRecord(RID rid, Tuple newRecord)
 	{
 		
 	}
-	
+
+	// must be done in O(log(n))
 	public void deleteRecord(RID rid)
 	{
 		
@@ -35,21 +70,16 @@ public class HeapFile {
 	
 	public int getRecCnt()
 	{
-		
+		return 0;
 	}
 	
 	public HeapScan openScan()
 	{
-		
+		return new HeapScan();
 	}
-}
-
-class RID
-{
-	
 }
 
 class Tuple
 {
-	
+	Byte[] tuple;
 }
