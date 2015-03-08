9d7ccf4f6b1d7f1194d4c4b9fd0e7686a546dae2
Phil Sands <psands@purdue.edu>
2015-03-07 19:59:38 -0500

    Created default behaviors for Tuple (unsure of overloaded constructor

--- HeapFile/src/heap/Tuple.java
+++ HeapFile/src/heap/Tuple.java
@@ -1,9 +1,32 @@
 package heap;
 
-public class Tuple {
-
-	public Tuple() {
-		// TODO Auto-generated constructor stub
+public class Tuple
+{
+	private Byte[] tuple;
+	
+	public Tuple()
+	{
+		tuple = new Byte[HFPage.PAGE_SIZE];
+	}
+	
+	public Tuple(Byte[] data, boolean IDK, int length)
+	{
+		tuple = new Byte[length];
+		int i = 0;
+		for (Byte b : data)
+		{
+			tuple[i++] = b;
+		}
+	}
+	
+	public int getLength()
+	{
+		return tuple.length;
+	}
+	
+	public Byte[] getTupleByteArray()
+	{
+		return tuple;
 	}
 
 }
