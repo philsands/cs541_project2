03ef673d7fc0151de9802e065dfd6b442b25f21f
Phil Sands <psands@purdue.edu>
2015-03-08 21:15:07 -0400

    Removed debugging code

--- HeapFile/src/heap/HeapFile.java
+++ HeapFile/src/heap/HeapFile.java
@@ -56,7 +56,7 @@ public class HeapFile {
 		{
 			insertPage = new HFPage();
 			rid = insertPage.insertRecord(record);
-			System.out.println(insertPage + "\n" + rid);
+			//System.out.println(insertPage + "\n" + rid);
 			bm.pinPage(rid.pageno, insertPage, false);
 			HFPages.add(insertPage);
 		}
